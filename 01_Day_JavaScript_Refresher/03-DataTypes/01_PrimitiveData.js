f3896ef0e3d47d4dfaa2c18d04d161efefe584c8
iamkapilll <kapildevchaudhary067@gmail.com>
2025-07-14 22:42:43 +0545

    Data Types in js

---
+++ 01_Day_JavaScript_Refresher/03-DataTypes/01_PrimitiveData.js
@@ -0,0 +1,27 @@
+// String
+let name = "Kapil";
+console.log(typeof name); // "string"
+
+// Number
+let age = 21;
+console.log(typeof age); // "number"
+
+// Boolean
+let isStudent = true;
+console.log(typeof isStudent); // "boolean"
+
+// Null
+let emptyValue = null;
+console.log(typeof emptyValue); // "object" (JavaScript quirk)
+
+// Undefined
+let notAssigned;
+console.log(typeof notAssigned); // "undefined"
+
+// BigInt
+let bigNumber = 1234567890123456789012345678901234567890n;
+console.log(typeof bigNumber); // "bigint"
+
+// Symbol
+let uniqueId = Symbol("id");
+console.log(typeof uniqueId); // "symbol"
---
+++ 01_Day_JavaScript_Refresher/03-DataTypes/02_NonPrimitiveData.js
@@ -0,0 +1,16 @@
+// Object
+let person = {
+  name: "Kapil",
+  age: 21
+};
+console.log(typeof person); // "object"
+
+// Array
+let numbers = [10, 20, 30];
+console.log(typeof numbers); // "object" (arrays are special objects)
+
+// Function
+function greet() {
+  return "Hello!";
+}
+console.log(typeof greet); // "function"
