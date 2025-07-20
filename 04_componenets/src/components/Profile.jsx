a67e7bac0731f30f816efe148a8c37d5d8466e36
iamkapilll <kapildevchaudhary067@gmail.com>
2025-07-20 08:56:12 +0545

    my first component(export and import basics)

---
+++ 04_componenets/src/components/Profile.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Profile() {
+
+  return (
+    <div className='flex  w-50 h-50 gap-0.5 p-3 mb-5' ><img src="https://media.licdn.com/dms/image/v2/D4D03AQH5LPeOtmhr3g/profile-displayphoto-crop_800_800/B4DZfGZDTKGYAI-/0/1751380146947?e=1755734400&v=beta&t=0L92YGCxoPrAPx7vG4HecvfLQoKEqLuzxYUZ_QqB6kY" alt="i am kapil" /></div>
+  )
+}
+
+export default Profile
